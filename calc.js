7818af1263b8bf5fa07b03990c56a8f92d88bdd6
srujana345 <sai.srujana144@gmail.com>
2025-03-06 10:46:49 +0530

    new

---
+++ calc.js
@@ -0,0 +1,12 @@
+
+ function add(a,b){
+    return a+b
+}
+
+ function subtract(a,b){
+    return a-b
+ }
+  function mul(a,b){
+    return a*b
+ }
+ export{add,subtract,mul}
---
+++ main.js
@@ -0,0 +1,7 @@
+import {add,subtract,mul} from "./calc.js"
+let result=add(2,3)
+console.log(result)
+let res1=subtract(9,2)
+console.log(res1)
+let res2=mul(9,2)
+console.log(res2)
